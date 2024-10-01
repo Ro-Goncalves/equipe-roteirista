5448e776dcbb0df56996e9202a6fccf0f277caa8
Rodrigo Gonçalves <53356279+Ro-Goncalves@users.noreply.github.com>
2024-09-30 23:49:56 -0300

    Update streamlit_app.py
    Removendo import o sqlite3

--- equipe_roteirista/streamlit_app.py
+++ equipe_roteirista/streamlit_app.py
@@ -1,7 +1,3 @@
-import pysqlite3
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 import streamlit as st
 from datetime import datetime
 from equipe.roteirista_execute import RoteiristaController
